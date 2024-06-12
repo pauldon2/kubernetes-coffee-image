55c88506e5a0d0e6abc06f93ea334b6ccf191325
Paul Shevtsov <paulshevtsov@gmail.com>
2024-06-12 10:45:27 +0300

    First commit

---
+++ evil-tea/src/run.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template
+from kubernetes import client, config
+
+app = Flask(__name__, static_url_path='/static', static_folder='static')
+
+def get_all_pods():
+    # Charger la configuration Kubernetes à partir du fichier kubeconfig par défaut
+    config.load_incluster_config()
+
+    # Créer un objet API client Kubernetes
+    api_instance = client.CoreV1Api()
+
+    # Récupérer tous les pods
+    pods = api_instance.list_pod_for_all_namespaces(watch=False)
+    
+    return pods
+
+@app.route('/')
+def index():
+    # Récupérer la liste des pods de tous les namespaces
+    try:
+        pods = get_all_pods()
+    except Exception as e:
+        return f"Error: {e}"
+        
+    # Formater les données pour l'affichage dans le template HTML
+    formatted_pods = []
+    for pod in pods.items:
+        formatted_pods.append({
+            'namespace': pod.metadata.namespace,
+            'name': pod.metadata.name,
+            'status': pod.status.phase,
+            'ip': pod.status.pod_ip
+        })
+
+    # Rendre le template HTML avec les données des pods
+    return render_template('index.html', pods=formatted_pods)
+
+if __name__ == '__main__':
+    # Listen on all IPs
+    app.run(host='0.0.0.0', debug=True)
---
+++ micro-service/frontend/index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Café !</title>
+    <link rel="stylesheet" href="css/style.css">
+
+    <style>
+        .cup-container {
+            text-align: center;
+            margin-top: 20px;
+            margin-bottom: 20px;
+        }
+
+        .ip-hostname, h1 {
+            font-family: Geneva, Tahoma, Verdana, sans-serif;
+        }
+
+        body {
+            height: 100%;
+            width: 100%;
+        }
+
+        .center {
+            display: block;
+            margin-left: auto;
+            margin-right: auto;
+            width: 50%;
+        }
+    </style>
+</head>
+
+<body>
+    <center>
+        <br>
+        <div class="cup-container center" style="padding-top: 5%">
+            <div class="cup">
+                <span class="steam "></span>
+                <span class="steam "></span>
+                <span class="steam "></span>
+                <div class="cup-handle "></div>
+            </div style="margin-bottom: 10px;">
+        </div>
+
+
+        <?php
+        $api_url = $_ENV["COFFEE_SERVER_API"];
+
+        $ch = curl_init($api_url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
+        $response = curl_exec($ch);
+        curl_close($ch);
+
+        $json_data = json_decode($response, true);
+
+        if (isset($json_data['number'])) {
+            $number = $json_data['number'];
+            echo "<h1> Number of coffees allowed today : " . $number . " </h1>";
+        } else {
+            echo "<p>Unable to fetch data from API</p>";
+        }
+        ?>
+
+        <footer style="padding-top: 10px">
+            <div class="ip-hostname">
+                <p>IP Address:
+                <?php echo getenv('POD_IP') ?: $_SERVER['SERVER_ADDR']; ?>
+                </p>
+                <p>Pod:
+                    <?php echo gethostname(); ?>
+                </p>
+            </div>
+        </footer>
+    </center>
+</body>
+
+</html>
